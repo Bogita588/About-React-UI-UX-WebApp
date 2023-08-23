eb686f68df7247e299cfeaf98b8edf8b61dd266a
Bogita Brian <bogitabrian@gmail.com>
2023-08-24 00:39:31 +0300

    Created components, fixed a bug that prevented them from rendering in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 
 
-import React from 'react'
+import React from 'react';
+import { Navbar, Brand, Feature, Cta} from './components';
+import {Footer, Blog, Possibility,  WhatGP3, Header } from './containers'
 
-function App() {
-  
-
+ const App = () => {
   return (
-    <>
-      
-    </>
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGP3 />
+      <Feature />
+      <Possibility />
+      <Cta />
+      <Blog />
+      <Footer />
+
+
+
+    </div>
   )
 }
-
 export default App
---
+++ src/containers/index.js
@@ -0,0 +1,6 @@
+export { default as Blog } from './Blog/Blog';
+export { default as Footer } from './Footer/Footer';
+export { default as Header } from './Header/Header';
+export { default as Possibility } from './Possibility/Possibility';
+export { default as WhatGP3 } from './WhatGP3/WhatGP3';
+
