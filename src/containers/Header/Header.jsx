f99287e8ac439f8be1a5623f8c0f8c5ee79cec14
Bogita Brian <bogitabrian@gmail.com>
2023-09-01 00:03:37 +0300

    Added new code to the navbar component and CSS for responsiveness in different screen sizes

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Header.css';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className='gpt3__header section__padding' id='home'>
+      <div className="gpt3_header-content">
+        <h1>Let us Now Build Something Amazing with React Library</h1>
+        <div className="gpt3__header-content__input">
+          <input type="email" placeholder="Please type your email" />
+          <button type="button">Get Started</button>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
