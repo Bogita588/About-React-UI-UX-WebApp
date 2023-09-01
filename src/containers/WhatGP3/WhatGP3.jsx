3c5a48fbe556bcef0061e98ec18ce04b501edce1
Bogita Brian <bogitabrian@gmail.com>
2023-09-01 13:30:35 +0300

    Added a component to the UI and styled it

--- src/containers/WhatGP3/WhatGP3.jsx
+++ src/containers/WhatGP3/WhatGP3.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import {Feature} from '../../components'
+import './whatGPT3.css'
 
 const WhatGP3 = () => {
   return (
-    <div>WhatGP3</div>
+
+    <div className="gpt3__whatgpt3 section__margin" id="whpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature />
+      
+
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className='gradient-text'>React feautures are beyond your Imagination </h1>
+        <p>Find out more about React</p>
+      </div>
+      <div className="gpt__whatgpt3-container">
+        <Feature />
+        <Feature />
+        <Feature />
+
+      </div>
+
+    </div>
   )
 }
 
